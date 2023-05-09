ba9c6517cbc2426c7dc65dedde4f55a686eb0976
fluteroute <ashley@ashleyndavid.com>
2023-05-08 22:36:39 -0400

    Added percy tests

---
+++ .percy/snapshots.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Building ashleyndavid.com"
+yarn build && yarn global add serve
+wait
+echo
+echo "Run ashleyndavid.com and take snapshots"
+serve -s build -l 3000 &
+yarn percy &&
+  killall node
